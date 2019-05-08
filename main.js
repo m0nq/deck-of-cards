30d5f240da6bd06127c28b2846f90a3ead449d50
Monk Wellington <forgetmenaught@gmail.com>
2019-05-08 13:35:30 -0700

    Beginning of building out Game class

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
-// Game
-//  Info section
-//  Deck
-//  Discard Pile
-//  Rules
+(window => {
+  // Game
+  class Game {
+    constructor(el, options) {
+      this.el = document.querySelector(el);
+      this.options = options;
+    }
+  }
+  //  Info section
+  //  Deck
+  //  Discard Pile
+  //  Rules
 
-// Deck
-//  Cards
-// -----
-//  shuffle
-//  stack
+  // Deck
+  //  Cards
+  // -----
+  //  shuffle
+  //  stack
 
-// Cards
-//  val
-//  suit
-//  ----
-//  flip
+  // Cards
+  //  val
+  //  suit
+  //  ----
+  //  flip
 
-// Discard Pile
-//  Holders
-//  -----
-//  accept or reject
+  // Discard Pile
+  //  Holders
+  //  -----
+  //  accept or reject
+  window.Game = Game;
+})(window);
