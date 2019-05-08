7bd7d1538b991b173acc76f7defc5eda60cf6cb6
Monk Wellington <forgetmenaught@gmail.com>
2019-05-08 13:19:27 -0700

    Psuedo code the plan for the games

---
+++ main.js
@@ -0,0 +1,22 @@
+// Game
+//  Info section
+//  Deck
+//  Discard Pile
+//  Rules
+
+// Deck
+//  Cards
+// -----
+//  shuffle
+//  stack
+
+// Cards
+//  val
+//  suit
+//  ----
+//  flip
+
+// Discard Pile
+//  Holders
+//  -----
+//  accept or reject
